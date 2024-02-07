fbe864153708c8ab5bda0b8141e4605a77ea8140
nickAmos <132029265+nickAmos@users.noreply.github.com>
2024-02-07 13:37:56 +1100

    Style: positioning of start page

--- poke-streak/src/Components/Home.jsx
+++ poke-streak/src/Components/Home.jsx
@@ -1,19 +1,43 @@
 import {useNavigate} from 'react-router-dom';
-
+import '../Style/Home.css';
 export default function Home ({changeTheme, styleHome}) {
 
     const navigation = useNavigate()
 
     return(
-        <div style={styleHome}>
-        <div>
-            <div onClick={() => changeTheme('turtwig')}>turtwig</div>
-            <div onClick={() => changeTheme('piplup')}>piplup</div>
-            <div onClick={() => changeTheme('chimchar')}>chimchar</div>
-        </div>
-        <button onClick={() => {
-            navigation('/MainGame')
-        }}>Go to game</button>
+        <div style={styleHome} id='Home-mainsection'>
+            <div id='left-side'>
+                <div id='theme-name-container'>
+                    <div id='change-theme-txt'>
+                        <p>Change theme below</p>
+                    </div>
+                    <div id='poketheme-container'>
+                        <div id='theme-child' onClick={() => changeTheme('turtwig')}>turtwig</div>
+                        <div id='theme-child' onClick={() => changeTheme('piplup')}>piplup</div>
+                        <div id='theme-child' onClick={() => changeTheme('chimchar')}>chimchar</div>
+                    </div>
+                </div>
+            </div>
+
+            <div id='right-side'>
+                <div id='button-txt-container'>
+                    <div id='button-container'>
+                            button will fill this space
+                    </div>
+                    {/*only display number high score when width is low*/}
+                    <div id='HighScore-text-container'>
+                            high score here
+                    </div>
+                </div>
+         
+            </div>
         </div>
     )
 }
+
+
+
+/*   OLD BUTTON
+ <button onClick={() => {
+                navigation('/MainGame')
+            }}>Go to game</button> */
